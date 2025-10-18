7592e6eb1882b27a007fd1e48267ac503d759e09
oscarigo <127225624+oscaringo243@users.noreply.github.com>
2025-10-18 11:43:34 +0200

    initial commit

---
+++ packages/server/index.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import type { Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+    res.json({ message: 'Hello World!' });
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port http://localhost:${PORT}`);
+});
